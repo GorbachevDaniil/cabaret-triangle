0b58637ea650952932783ccc897885d0c1e69273
daniil <gorbachev-daniil@inbox.ru>
2018-02-17 10:00:01 +0300

    Fixed PolyGenerator

--- triangle/PolyGenerator.cpp
+++ triangle/PolyGenerator.cpp
@@ -4,36 +4,38 @@
 
 #define L_x 1.0
 #define L_y 1.0
-#define N_x 5.0
-#define N_y 5.0
+#define N_x 100.0
+#define N_y 100.0
 
 using namespace std;
 
 int main()
 {
 	int node_index = 1;
-	double h_x = 2 * L_x / N_x;
-	double h_y = 2 * L_y / N_y;
-	ofstream polyfile("Mesh.poly");
-	polyfile << (N_x-1)*(N_y-1)+4 << " 2 0 0" << endl;
+	double h_x = 2 * L_x / (N_x - 1);
+	double h_y = 2 * L_y / (N_y - 1);
+	int number_of_dots = 2 * (N_x + N_y) - 4;
 
-	for (int i = 0; i < N_y ; i++) {
+	ofstream polyfile("Mesh.poly");
+	polyfile << number_of_dots << " 2 0 0" << endl;
+
+	for (int i = 0; i < N_y - 1; i++) {
 		polyfile << node_index << " " <<-L_x << " " << -L_y + i * h_y << endl;
 		node_index++;
 	}
-	for (int i = 0; i < N_x; i++) {
+	for (int i = 0; i < N_x - 1; i++) {
 		polyfile << node_index << " " << -L_x + i * h_x << " " << L_y << endl;
 		node_index++;
 	}
-	for (int i = N_y; i > 0; i--) {
+	for (int i = N_y - 1; i > 0; i--) {
 		polyfile << node_index << " " << L_x << " " << -L_y + i * h_x << endl;
 		node_index++;
 	}
-	for (int i = N_x; i > 0; i--) {
+	for (int i = N_x - 1; i > 0; i--) {
 		polyfile << node_index << " " << -L_x + i * h_x << " " << -L_y << endl;
 		node_index++;
 	}
-	polyfile << (N_x - 1)*(N_y - 1) + 4 << " 0" << endl;
+	polyfile << number_of_dots << " 0" << endl;
 	for (int i = 1; i < node_index-1; i++) {
 		polyfile << i << " " << i << " " << i+1 << endl;
 	}
