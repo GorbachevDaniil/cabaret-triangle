d0b015648a340b8395be1a8f4f63b7ddc831d993
daniil <gorbachev-daniil@inbox.ru>
2018-02-18 22:05:11 +0300

    Wrote tests for getEdgeOrderedNodeIDs in Cell class

--- test/TestCell.cpp
+++ test/TestCell.cpp
@@ -2,6 +2,8 @@
 
 #include "Cell.hpp"
 
+#include <vector>
+
 TEST(getNextNodeIDTest, ReturnTheNextValueInACircularManner) {
     Cell cell;
     cell.nodeIDs.push_back(10);
@@ -34,4 +36,63 @@ TEST(getPrevNodeIDTest, DieIfIncomingNodeIDPosMoreThanSize) {
     Cell cell;
 
     ASSERT_DEATH(cell.getNextNodeID(1), "");
+}
+
+TEST(getEdgeOrderedNodeIDs, UnorderedNodeIDs) {
+    Cell cell;
+    cell.nodeIDs.push_back(10);
+    cell.nodeIDs.push_back(11);
+    cell.nodeIDs.push_back(12);
+
+    std::vector<long> unorderedNodeIDs;
+    unorderedNodeIDs.push_back(10);
+    unorderedNodeIDs.push_back(12);
+
+    std::vector<long> orderedNodeIDs = cell.getEdgeOrderedNodeIDs(unorderedNodeIDs);
+    EXPECT_EQ(12, orderedNodeIDs[0]);
+    EXPECT_EQ(10, orderedNodeIDs[1]);
+}
+
+TEST(getEdgeOrderedNodeIDs, OrderedNodeIDs) {
+    Cell cell;
+    cell.nodeIDs.push_back(10);
+    cell.nodeIDs.push_back(11);
+    cell.nodeIDs.push_back(12);
+
+    std::vector<long> unorderedNodeIDs;
+    unorderedNodeIDs.push_back(12);
+    unorderedNodeIDs.push_back(10);
+
+    std::vector<long> orderedNodeIDs = cell.getEdgeOrderedNodeIDs(unorderedNodeIDs);
+    EXPECT_EQ(12, orderedNodeIDs[0]);
+    EXPECT_EQ(10, orderedNodeIDs[1]);
+}
+
+TEST(getEdgeOrderedNodeIDs, SizeMustBeTwoForIncomingVector) {
+    Cell cell;
+
+    std::vector<long> unorderedNodeIDs;
+    unorderedNodeIDs.push_back(10);
+    unorderedNodeIDs.push_back(11);
+    unorderedNodeIDs.push_back(12);
+
+    ASSERT_DEATH(cell.getEdgeOrderedNodeIDs(unorderedNodeIDs), "");
+
+    unorderedNodeIDs.clear();
+    unorderedNodeIDs.push_back(10);
+
+    ASSERT_DEATH(cell.getEdgeOrderedNodeIDs(unorderedNodeIDs), "");
+}
+
+TEST(getEdgeOrderedNodeIDs, SizeMustBeTwoForOutcomingVector) {
+    Cell cell;
+    cell.nodeIDs.push_back(10);
+    cell.nodeIDs.push_back(11);
+    cell.nodeIDs.push_back(12);
+
+    std::vector<long> unorderedNodeIDs;
+    unorderedNodeIDs.push_back(11);
+    unorderedNodeIDs.push_back(11);
+
+    ASSERT_DEATH(cell.getEdgeOrderedNodeIDs(unorderedNodeIDs), "");
 }
